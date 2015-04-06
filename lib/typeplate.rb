da396dbb89e851d7bba900d0adc7724be8b02bc1
Pablo Olmos de Aguilera Corradini <pablo+git@odac.co>
2015-04-05 22:48:04 -0300

    Add typeplate to SASS_PATH

--- lib/typeplate.rb
+++ lib/typeplate.rb
@@ -1,5 +1,11 @@
-require "typeplate/version"
+require 'typeplate/version'
+
+unless defined?(Sass)
+  require 'sass'
+end
 
 module Typeplate
-  # Your code goes here...
+  typeplate_path = File.expand_path("../../app/assets/stylesheets", __FILE__)
+
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], typeplate_path].compact.join(File::PATH_SEPARATOR)
 end
